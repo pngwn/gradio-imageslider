73017f85341177ba48ae7f843a28d58b437d3a70
pngwn <hello@pngwn.io>
2023-12-07 23:28:55 +0000

    changes

--- demo/app.py
+++ demo/app.py
@@ -1,19 +1,10 @@
 import gradio as gr
 from gradio_imageslider import ImageSlider
-from pathlib import Path
-import os
 
 
 def fn(im):
     return im
 
-
-# get absolute path
-
-
-dir_path = Path(os.path.realpath(__file__)).parent / "images"
-
-print(dir_path)
 
 with gr.Blocks() as demo:
     img1 = ImageSlider()
@@ -21,16 +12,5 @@ with gr.Blocks() as demo:
     img2 = ImageSlider()
     btn.click(fn, inputs=img1, outputs=img2)
 
-    # gr.Examples(
-    #     [
-    #         (
-    #             Path("demo/images/puppy_color_opt.jpg"),
-    #             Path("demo/images/puppy_bw_opt.jpg"),
-    #         )
-    #     ],
-    #     img1,
-    #     label="Example images",
-    # )
-
-
-demo.launch()
+if __name__ == "__main__":
+    demo.launch()
